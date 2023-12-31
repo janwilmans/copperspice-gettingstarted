bf515a2507d1e6e8f28adebec4fff00808795fb6
Jan Wilmans <jan.wilmans@gmail.com>
2023-12-31 15:25:35 +0100

    fixed example_project_cmake to work entire zero-configuration, just run build.bat

--- example_project_cmake/build.bat
+++ example_project_cmake/build.bat
@@ -1,10 +1,7 @@
-set CS_RELEASE_PATH=C:/project/internal-copperspice/package/nuget/copperspice.1.8.2/build/native/release/cmake
-set CS_DEBUG_PATH=C:/project/internal-copperspice/package/nuget/copperspice.1.8.2/build/native/debug/cmake
-
 set CMAKE_ARGS=-DCMAKE_INSTALL_PREFIX=install -DMSVC_TOOLSET_VERSION=143 -G Ninja
 
-cmake -DCMAKE_PREFIX_PATH=%CS_DEBUG_PATH% %CMAKE_ARGS% -DCMAKE_BUILD_TYPE=Debug -Bbuild/debug
+cmake %CMAKE_ARGS% -DCMAKE_BUILD_TYPE=Debug -Bbuild/debug
 cmake --build build/debug --target install
 
-::cmake -DCMAKE_PREFIX_PATH=%CS_RELEASE_PATH% %CMAKE_ARGS% -DCMAKE_BUILD_TYPE=RelWithDebInfo -Bbuild/release
+::cmake %CMAKE_ARGS% -DCMAKE_BUILD_TYPE=RelWithDebInfo -Bbuild/release
 ::cmake --build build/release --target install
