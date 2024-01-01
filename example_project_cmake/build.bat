dee17c425347942ae183a9b5815b80dcda12f42c
Jan Wilmans <jan.wilmans@gmail.com>
2024-01-01 16:15:03 +0100

    set better defaults

--- example_project_cmake/build.bat
+++ example_project_cmake/build.bat
@@ -1,7 +1,5 @@
-set CMAKE_ARGS=-DCMAKE_INSTALL_PREFIX=install -DMSVC_TOOLSET_VERSION=143 -G Ninja
-
-cmake %CMAKE_ARGS% -DCMAKE_BUILD_TYPE=Debug -Bbuild/debug
+cmake -DMSVC_TOOLSET_VERSION=143 -G Ninja -DCMAKE_BUILD_TYPE=Debug -Bbuild/debug
 cmake --build build/debug --target install
 
-::cmake %CMAKE_ARGS% -DCMAKE_BUILD_TYPE=RelWithDebInfo -Bbuild/release
-::cmake --build build/release --target install
+cmake -DMSVC_TOOLSET_VERSION=143 -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -Bbuild/release
+cmake --build build/release --target install
