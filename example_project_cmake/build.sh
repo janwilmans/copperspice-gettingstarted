62db796b5b3c386930b56652792991d23309149d
Jan Wilmans <jan.wilmans@vimec.nl>
2024-01-06 15:51:48 +0100

    fixed lineendings and added asan/ubsan in cmakefile

--- example_project_cmake/build.sh
+++ example_project_cmake/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -Bbuild/debug
+export CopperSpice_DIR=/home/jan.wilmans/project/copperspice/install/lib/cmake/CopperSpice
+
+cmake -G Ninja -DBUILD_WITH_UBSAN=ON -DCMAKE_BUILD_TYPE=Debug -Bbuild/debug
 cmake --build build/debug --target install
 
 cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -Bbuild/release
